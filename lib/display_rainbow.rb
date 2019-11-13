e91009dd18f946ae74ba379672fba0681de1d573
Alethea Cary <aletheacary@gmail.com>
2019-11-13 12:26:14 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,12 +2,12 @@
 require 'pry'
 def display_rainbow(colors)
   counter = 1
-  puts colors.each do |color|
-    if (counter) == colors.length
-      "#{color.split(//)[0].upcase}: #{color}"
-    else
-       "#{color.split(//)[0].upcase}: #{color}, "
-      counter +=1
-    end
+  colors.each do |color|
+    # if (counter) == colors.length
+    #   print "#{color.split(//)[0].upcase}: #{color}"
+    # else
+    #    print "#{color.split(//)[0].upcase}: #{color}, "
+    #   counter +=1
+    # end
   end
 end
