385151ce61142dd738a24a99f0a7d351e19b16a0
Alethea Cary <aletheacary@gmail.com>
2019-11-13 12:10:15 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,7 +4,7 @@ def display_rainbow(colors)
   counter = 1
   colors.each do |color|
     if (counter) == colors.length
-      print "#{color.split(//)[0].upcase}: #{color}"
+      print "#{color.split(//)[0].upcase}: #{color}\n"
     else
       print "#{color.split(//)[0].upcase}: #{color}, "
       counter +=1
@@ -12,3 +12,8 @@ def display_rainbow(colors)
 
   end
 end
+
+# "R: red, O: orange, Y: yellow, G
+# : green, B: blue, I: indigo, V: violet\n" to stdout, but output
+# "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V:
+# violet"
